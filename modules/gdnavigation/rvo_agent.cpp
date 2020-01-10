7e397bf8b3b1a046a28d587b6724d0695fa6ce8a
Andrea Catania <info@andreacatania.com>
2020-01-10 12:22:34 +0100

    - Integrated NavigationServer and Navigation2DServer. - Added Navigation Agents and Obstacles. - Integrated Collision Avoidance.
    This work has been kindly sponsored by IMVU.

--- modules/gdnavigation/rvo_agent.cpp
+++ modules/gdnavigation/rvo_agent.cpp
@@ -1,12 +1,12 @@
 /*************************************************************************/
-/*  register_types.h                                                     */
+/*  rvo_agent.cpp                                                        */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
 /*                      https://godotengine.org                          */
 /*************************************************************************/
 /* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md)    */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -28,5 +28,57 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-void register_recast_types();
-void unregister_recast_types();
+#include "rvo_agent.h"
+
+#include "nav_map.h"
+
+/**
+	@author AndreaCatania
+*/
+
+RvoAgent::RvoAgent() :
+		map(NULL) {
+	callback.id = ObjectID(0);
+}
+
+void RvoAgent::set_map(NavMap *p_map) {
+	map = p_map;
+}
+
+bool RvoAgent::is_map_changed() {
+	if (map) {
+		bool is_changed = map->get_map_update_id() != map_update_id;
+		map_update_id = map->get_map_update_id();
+		return is_changed;
+	} else {
+		return false;
+	}
+}
+
+void RvoAgent::set_callback(ObjectID p_id, const StringName p_method, const Variant p_udata) {
+	callback.id = p_id;
+	callback.method = p_method;
+	callback.udata = p_udata;
+}
+
+bool RvoAgent::has_callback() const {
+	return callback.id != 0;
+}
+
+void RvoAgent::dispatch_callback() {
+	if (callback.id == 0) {
+		return;
+	}
+	Object *obj = ObjectDB::get_instance(callback.id);
+	if (obj == NULL) {
+		callback.id = ObjectID(0);
+	}
+
+	Variant::CallError responseCallError;
+
+	callback.new_velocity = Vector3(agent.newVelocity_.x(), agent.newVelocity_.y(), agent.newVelocity_.z());
+
+	const Variant *vp[2] = { &callback.new_velocity, &callback.udata };
+	int argc = (callback.udata.get_type() == Variant::NIL) ? 1 : 2;
+	obj->call(callback.method, vp, argc, responseCallError);
+}
